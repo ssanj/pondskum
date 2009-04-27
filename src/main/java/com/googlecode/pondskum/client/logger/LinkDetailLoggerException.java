edde17f8fde58e44ad0e324008149dfb0bed1b2d
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-04-27 05:45:48 +0000

    adding proper package structure.

---
+++ src/main/java/com/googlecode/pondskum/client/logger/LinkDetailLoggerException.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pondskum.client.logger;
+
+public final class LinkDetailLoggerException extends RuntimeException {
+
+    private static final long serialVersionUID = 3839522759978247399L;
+
+    public LinkDetailLoggerException(final String message) {
+        super(message);
+    }
+
+    public LinkDetailLoggerException(final Throwable cause) {
+        super(cause);
+    }
+
+
+    public LinkDetailLoggerException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
