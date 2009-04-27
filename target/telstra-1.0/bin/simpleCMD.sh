d0f9b7a1c561392c9581f7b6e08aa5e2082c2a73
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-04-27 02:15:53 +0000

    removed target directory

--- target/telstra-1.0/bin/simpleCMD.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-echo "Bigpond file location=$BIGPOND_CONFIG_FILE_LOCATION"
-echo "classpath=$JAVA_CMD_CP"
-
-java -Dbigpond.config.location=$BIGPOND_CONFIG_FILE_LOCATION -classpath $JAVA_CMD_CP com.googlecode.teltra.gui.simplecmd.SimpleCMD 
