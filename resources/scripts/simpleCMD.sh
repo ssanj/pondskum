0cf5294b29d61f7395c458e096d27ed2e3452d6d
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-04-28 10:28:06 +0000

    1. renamed ponscum package -> pondskum 2. created stubby.

--- resources/scripts/simpleCMD.sh
+++ resources/scripts/simpleCMD.sh
@@ -3,4 +3,4 @@
 echo "Bigpond file location=$BIGPOND_CONFIG_FILE_LOCATION"
 echo "classpath=$JAVA_CMD_CP"
 
-java -Dbigpond.config.location=$BIGPOND_CONFIG_FILE_LOCATION -classpath $JAVA_CMD_CP com.googlecode.pondscum.gui.simplecmd.SimpleCMD
+java -Dbigpond.config.location=$BIGPOND_CONFIG_FILE_LOCATION -classpath $JAVA_CMD_CP com.googlecode.pondskum.gui.simplecmd.SimpleCMD
