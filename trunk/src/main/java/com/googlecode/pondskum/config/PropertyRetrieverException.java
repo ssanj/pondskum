3dab2dce9d6d5ff1305dff0950e778a0698eaeda
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-04-28 11:35:38 +0000

    1. renamed ponscum package -> pondskum

--- trunk/src/main/java/com/googlecode/pondskum/config/PropertyRetrieverException.java
+++ trunk/src/main/java/com/googlecode/pondskum/config/PropertyRetrieverException.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.googlecode.teltra.config;
+package com.googlecode.pondskum.config;
 
 public final class PropertyRetrieverException extends RuntimeException {
 
