9bf60244277f784f6b7defddec22ac89f8aeecb5
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-04-30 08:54:20 +0000

    1. Updated scripts

--- trunk/resources/scripts/tablet.sh
+++ trunk/resources/scripts/tablet.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Bigpond file location=$BIGPOND_CONFIG_FILE_LOCATION"
-$JAVA_CMD_CP=$JAVA_CMD_CP:core-8.9732.jar
+export JAVA_CMD_CP=$JAVA_CMD_CP:../lib/core-8.9732.jar
 echo "classpath=$JAVA_CMD_CP"
 
 java -Dbigpond.config.location=$BIGPOND_CONFIG_FILE_LOCATION -classpath $JAVA_CMD_CP com.googlecode.pondskum.gui.swing.tablet.TabletRunner
