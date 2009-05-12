b4165753f266b5d8b807bf6ab5b77cd44424b1ac
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-05-12 09:58:33 +0000

    1. Added TableHeaderRender.

---
+++ src/main/java/com/googlecode/pondskum/gui/swing/tablet/TableHeaderRenderer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2008 Sanjiv Sahayam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.pondskum.gui.swing.tablet;
+
+import sun.swing.table.DefaultTableCellHeaderRenderer;
+
+import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+public final class TableHeaderRenderer extends DefaultTableCellHeaderRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        component.setFont(new Font("DejaVu Sans", Font.BOLD, 16));
+        component.setBackground(new Color(131, 203, 146));
+        return component;
+    }
+}
