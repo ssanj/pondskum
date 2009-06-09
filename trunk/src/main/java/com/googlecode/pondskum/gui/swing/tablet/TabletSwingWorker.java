093684bd0e3c937f1aa8e0c1f71a75267efd9141
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-06-09 11:47:20 +0000

    1. Added StopWatch. Removed Timer.

--- trunk/src/main/java/com/googlecode/pondskum/gui/swing/tablet/TabletSwingWorker.java
+++ trunk/src/main/java/com/googlecode/pondskum/gui/swing/tablet/TabletSwingWorker.java
@@ -15,12 +15,13 @@
  */
 package com.googlecode.pondskum.gui.swing.tablet;
 
+import com.googlecode.pinthura.util.StopWatch;
 import com.googlecode.pinthura.util.SystemPropertyRetrieverImpl;
+import com.googlecode.pinthura.util.builder.StopWatchBuilder;
 import com.googlecode.pondskum.client.BigpondConnector;
 import com.googlecode.pondskum.client.BigpondConnectorImpl;
 import com.googlecode.pondskum.client.BigpondUsageInformation;
 import com.googlecode.pondskum.config.ConfigFileLoaderImpl;
-import com.googlecode.pondskum.util.Timer;
 
 import javax.swing.SwingWorker;
 import java.util.List;
@@ -29,18 +30,18 @@ import java.util.Properties;
 public final class TabletSwingWorker extends SwingWorker<BigpondUsageInformation, String> implements StatusUpdatable {
 
     private final UpdatableTablet tablet;
-    private Timer timer;
+    private StopWatch stopWatch;
 
     TabletSwingWorker(final UpdatableTablet tablet) {
         this.tablet = tablet;
-        timer = new Timer();
+        stopWatch = new StopWatchBuilder().build();
     }
 
     @Override
     protected BigpondUsageInformation doInBackground() throws Exception {
         Properties properties = new ConfigFileLoaderImpl(new SystemPropertyRetrieverImpl()).loadProperties("bigpond.config.location");
         BigpondConnector bigpondConnector = new BigpondConnectorImpl(properties);
-        timer.start();
+        stopWatch.start();
         return bigpondConnector.connect(new ConsoleConnectionListener(this));
     }
 
@@ -54,9 +55,8 @@ public final class TabletSwingWorker extends SwingWorker<BigpondUsageInformation
     @Override
     protected void done() {
         try {
-            long timeTaken = timer.getTime();
             tablet.setTabletData(get());
-            tablet.updateStatus("Time taken: " + (timeTaken / 1000) + " seconds");
+            tablet.updateStatus("Time taken: " + stopWatch.getTimeInSeconds() + " seconds");
         } catch (Exception e) {
             e.printStackTrace();
         }
