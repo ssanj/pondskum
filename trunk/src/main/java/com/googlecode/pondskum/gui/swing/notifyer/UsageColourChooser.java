972355dda2575e1e5a4af36100261a0319db107a
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-09-12 06:38:23 +0000

    1.Refactored.

--- trunk/src/main/java/com/googlecode/pondskum/gui/swing/notifyer/UsageColourChooser.java
+++ trunk/src/main/java/com/googlecode/pondskum/gui/swing/notifyer/UsageColourChooser.java
@@ -15,36 +15,78 @@
  */
 package com.googlecode.pondskum.gui.swing.notifyer;
 
+import com.googlecode.pinthura.annotation.SuppressionReason;
+
 import java.awt.Color;
 import java.util.Arrays;
 import java.util.List;
 
 public final class UsageColourChooser {
 
-    private static final int PERCENTAGE = 100;
+    private static final int PERCENT = 100;
+    private static final int PERCENTAGE_FOR_RANGE_ONE_START = 0;
+    private static final int PERCENTAGE_FOR_RANGE_ONE_END = 25;
+    private static final int PERCENTAGE_FOR_RANGE_TWO_START = 26;
+    private static final int PERCENTAGE_FOR_RANGE_TWO_END = 50;
+    private static final int PERCENTAGE_FOR_RANGE_THREE_START = 51;
+    private static final int PERCENTAGE_FOR_RANGE_THREE_END = 75;
+    private static final int PERCENTAGE_FOR_RANGE_FOUR_START = 76;
+    private static final int PERCENTAGE_FOR_RANGE_FOUR_END = 90;
+    private static final int PERCENTAGE_FOR_RANGE_FIVE_START = 91;
+    private static final int PERCENTAGE_FOR_RANGE_FIVE_END = 100;
 
     private List<Range> ratiosList;
 
     public UsageColourChooser() {
-        // CHECKSTYLE:OFF
-        Range range1 = new Range(0, 25, new Color(0xAD,0xC5,0x13));//#ADC513
-        Range range2  = new Range(26, 50, new Color(0xFF, 0xCC, 0x05));//#FFCC05
-        Range range3 = new Range(51, 75, new Color(0xFF, 0x5F, 0x05));//#FF5F05
-        Range range4 = new Range(76, 90, new Color(0xC5, 0x28, 0x13));//#C52813
-        Range range5 = new Range(91, 100, new Color(0xFC, 0x24, 0x06));//#FC2406
-        // CHECKSTYLE:ON
+
+        Range range1 = new Range(PERCENTAGE_FOR_RANGE_ONE_START, PERCENTAGE_FOR_RANGE_ONE_END, createGreenColour());
+        Range range2  = new Range(PERCENTAGE_FOR_RANGE_TWO_START, PERCENTAGE_FOR_RANGE_TWO_END, createYellowColour());
+        Range range3 = new Range(PERCENTAGE_FOR_RANGE_THREE_START, PERCENTAGE_FOR_RANGE_THREE_END, createOrangeColour());
+        Range range4 = new Range(PERCENTAGE_FOR_RANGE_FOUR_START, PERCENTAGE_FOR_RANGE_FOUR_END, createDarkRed());
+        Range range5 = new Range(PERCENTAGE_FOR_RANGE_FIVE_START, PERCENTAGE_FOR_RANGE_FIVE_END, createBrightRed());
+
         ratiosList = Arrays.asList(range1, range2, range3, range4, range5);
     }
 
+    // CHECKSTYLE:OFF
+    private Color createBrightRed() {
+        return new Color(0xFC, 0x24, 0x06); //#FC2406
+    }
+
+    private Color createDarkRed() {
+        return new Color(0xC5, 0x28, 0x13); //#C52813
+    }
+
+    private Color createOrangeColour() {
+        return new Color(0xFF, 0x5F, 0x05); //#FF5F05
+    }
+
+    private Color createYellowColour() {
+        return new Color(0xFF, 0xCC, 0x05); //#FFCC05
+    }
+
+    private Color createGreenColour() {
+        return new Color(0xAD,0xC5,0x13); //#ADC513
+    }
+    // CHECKSTYLE:ON
+
+    @SuppressWarnings({"MethodParameterOfConcreteClass"})
+    @SuppressionReason(value = SuppressionReason.Reason.OTHER, desc = "Parameter is a private static final inner class.")    
     public Color getColor(double usageRatio) {
         for (Range range : ratiosList) {
-            if (range.isInRange((int) (usageRatio * PERCENTAGE))) {
+            if (hasColourForPercentageUsage(usageRatio, range)) {
                 return range.getColor();
             }
         }
 
         //this should never happen! :)
         return Color.WHITE;
+    }
+
+    @SuppressWarnings({"MethodParameterOfConcreteClass"})
+    @SuppressionReason(value = SuppressionReason.Reason.OTHER, desc = "Parameter is a private static final inner class.")
+    private boolean hasColourForPercentageUsage(final double usageRatio, final Range range) {
+        return range.isInRange((int) (usageRatio * PERCENT));
     }
 
     private static final class Range {
