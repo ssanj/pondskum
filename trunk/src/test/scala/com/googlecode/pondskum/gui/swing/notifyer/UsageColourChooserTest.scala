e58aab45ef6b0ec90406a03dadc3fd8d68da5b53
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-09-22 10:35:56 +0000

    1. Created additional context. 2. moved UsageColourChooserObject to its own class.

--- trunk/src/test/scala/com/googlecode/pondskum/gui/swing/notifyer/UsageColourChooserTest.scala
+++ trunk/src/test/scala/com/googlecode/pondskum/gui/swing/notifyer/UsageColourChooserTest.scala
@@ -16,70 +16,29 @@
 
 package com.googlecode.pondskum.gui.swing.notifyer
 
-import notifyer._
 import org.junit.Assert.assertThat
+import notifyer._
 import java.awt.Color
 import org.hamcrest.core.IsEqual.equalTo
 import org.junit.{Test}
-import org.junit.Assert.fail
-import pinthura.util.builder.RandomDataCreatorBuilder
+import UsageColourChooserObject._
 
 final class UsageColourChooserTest {
 
-  private val colourChooser = createColourChooser
-  private val randomDataCreator = createRandomDataCreator
+  private val colourChooser = new UsageColourChooser
 
-  @Test def shouldReturnGreenForARangeOfZeroToTwentyFivePercent { assertPercentageRangeWithColour(0, 25, greenColour) }
+  @Test def shouldReturnAGreenColourForARangeOfZeroToTwentyFivePercent { assertPercentageRangeWithColour(0, 25, greenColour) }
 
-  @Test def shouldReturnYellowForARangeOfTwentySixToFifty { assertPercentageRangeWithColour(26, 50, yellowColour) }
+  @Test def shouldReturnAYellowColourForARangeOfTwentySixToFifty { assertPercentageRangeWithColour(26, 50, yellowColour) }
 
-  @Test def shouldReturnOrangeForARangeOfFiftyOneToSeventyFive { assertPercentageRangeWithColour(51, 75, orangeColour) }
+  @Test def shouldReturnAnOrangeColourForARangeOfFiftyOneToSeventyFive { assertPercentageRangeWithColour(51, 75, orangeColour) }
 
-  @Test def shouldReturnDarkRedForARangeOfSeventySixOneToNinty { assertPercentageRangeWithColour(76, 90, darkRedColour) }
+  @Test def shouldReturnADarkRedColourForARangeOfSeventySixOneToNinety { assertPercentageRangeWithColour(76, 90, darkRedColour) }
 
-  @Test def shouldReturnBrightRedForARangeOfNintyOneToHundred { assertPercentageRangeWithColour(91, 100, brightRedColour) }
-
-  @Test
-  def shouldThrowAnExceptionForAnyOtherRange {
-    val randomPercentage = getRandomPercentage(101, 500)
-    try {
-      colourChooser getColor randomPercentage
-      fail("Expected UnknownUsagePercentageException for % of " + randomPercentage)
-    } catch {
-      case e : UnknownUsagePercentageException => assertThat("unhandled % of " + randomPercentage, equalTo(e.getMessage))
-    }
-  }
+  @Test def shouldReturnABrightRedColourForARangeOfNintyOneToHundred { assertPercentageRangeWithColour(91, 100, brightRedColour) } 
 
   private def assertPercentageRangeWithColour(min : Int, max : Int, expectedColor : Color) {
     val randomPercentage = getRandomPercentage(min, max)
     assertThat("failed with percentage of " + randomPercentage, expectedColor, equalTo(colourChooser getColor randomPercentage))
-  }
-
-  private def getRandomPercentage(min : Int, max : Int) = randomDataCreator.createBoundedNumber(min, plusOne(max)) *
-          UsageColourChooserTest.AS_PERCENTAGE;
-
-  private def greenColour = UsageColourChooserTest.GREEN
-
-  private def yellowColour = UsageColourChooserTest.YELLOW
-
-  private def orangeColour = UsageColourChooserTest.ORANGE
-
-  private def darkRedColour = UsageColourChooserTest.DARK_RED
-
-  private def brightRedColour = UsageColourChooserTest.BRIGHT_RED
-
-  private def plusOne(number : Int) = number + 1
-
-  private def createRandomDataCreator = new RandomDataCreatorBuilder build
-
-  private def createColourChooser = new UsageColourChooser
-
-  private object UsageColourChooserTest {
-    val AS_PERCENTAGE = 0.01
-    val GREEN = new Color(0xAD,0xC5,0x13)
-    val YELLOW = new Color(0xFF, 0xCC, 0x05)
-    val ORANGE = new Color(0xFF, 0x5F, 0x05)
-    val DARK_RED = new Color(0xC5, 0x28, 0x13)
-    val BRIGHT_RED = new Color(0xFC, 0x24, 0x06)
   }
 }
