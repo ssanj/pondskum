8249108dfcea77ff3d0cc1d3a830680666e9b2ef
sanjivsahayam <sanjivsahayam@7b2bd770-324a-11de-ba44-d58fe8a9ce33>
2009-10-24 08:36:29 +0000

    1. Updated pondskum version to 1.0.2

--- resources/scripts/runProgress.bat
+++ resources/scripts/runProgress.bat
@@ -17,6 +17,6 @@ REM limitations under the License.
 echo "setting configuration parameters"
 
 set BIGPOND_CONFIG_FILE_LOCATION=..\etc
-set JAVA_CMD_CP=..\etc;..\lib\httpclient-4.0-beta2.jar;..\lib\httpcore-4.0-beta3.jar;..\lib\commons-logging-1.1.1.jar;..\lib\commons-codec-1.3.jar;..\lib\htmlparser-1.6.jar;..\lib\pinthura-core-0.0.2-SNAPSHOT.jar;..\lib\cglib-nodep-2.1_3.jar;..\lib\slf4j-api-1.5.2.jar;..\lib\slf4j-simple-1.5.2.jar;..\lib\guice-2.0.jar;..\lib\aopalliance-1.0.jar;..\lib\scala-library-2.7.5.jar;..\lib\pondskum-1.0.1.jar;..\lib\core-8.9732.jar
+set JAVA_CMD_CP=..\etc;..\lib\httpclient-4.0-beta2.jar;..\lib\httpcore-4.0-beta3.jar;..\lib\commons-logging-1.1.1.jar;..\lib\commons-codec-1.3.jar;..\lib\htmlparser-1.6.jar;..\lib\pinthura-core-0.0.2-SNAPSHOT.jar;..\lib\cglib-nodep-2.1_3.jar;..\lib\slf4j-api-1.5.2.jar;..\lib\slf4j-simple-1.5.2.jar;..\lib\guice-2.0.jar;..\lib\aopalliance-1.0.jar;..\lib\scala-library-2.7.5.jar;..\lib\pondskum-1.0.2.jar;..\lib\core-8.9732.jar
 
 java -Dbigpond.config.location=%BIGPOND_CONFIG_FILE_LOCATION% -classpath %JAVA_CMD_CP% com.googlecode.pondskum.gui.swing.notifyer.ProgressionPanelRunner
